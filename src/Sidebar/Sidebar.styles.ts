62877b00c4eea2033e4b6a8291587dd32c72f19e
Yury Baykov <y.baykov@corp.mail.ru>
2019-06-13 02:10:22 +0300

    increased sidebar width

--- src/Sidebar/Sidebar.styles.ts
+++ src/Sidebar/Sidebar.styles.ts
@@ -3,7 +3,7 @@ import { createStyles, Theme } from '@material-ui/core';
 export default (theme: Theme) => createStyles({
 	container: {
 		height: '100%',
-		width: 200,
+		width: 220,
 		borderRight: `1px solid ${theme.palette.divider}`
 	}
 });
