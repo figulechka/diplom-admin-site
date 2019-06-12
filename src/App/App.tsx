bdac8357d5fb6dbe5ae06021b2d07c7b3226fdaf
Yury Baykov <y.baykov@corp.mail.ru>
2019-06-12 22:13:33 +0300

    seem like header, but an ugly one

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -5,8 +5,22 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import withStyles, { WithStyles } from '@material-ui/core/styles/withStyles';
+import { createStyles, Theme } from '@material-ui/core';
 
-interface Props {
+const styles = ({ spacing }: Theme) => createStyles({
+	root: {
+		flexGrow: 1
+	},
+	menuButton: {
+		marginRight: spacing(2)
+	},
+	title: {
+		flexGrow: 1
+	}
+});
+
+interface Props extends WithStyles<typeof styles> {
 
 }
 
@@ -14,7 +28,7 @@ interface State {
 	version: number;
 }
 
-export default class App extends React.Component<Props, State> {
+class App extends React.Component<Props, State> {
 	public state: State = {
 		version: 101
 	};
@@ -30,25 +44,31 @@ export default class App extends React.Component<Props, State> {
 	private readonly onPublishClick = () => this.setState({ version: this.state.version + 1 });
 
 	public render(): React.ReactElement {
+		const { classes } = this.props;
+
 		return (
 			<div>
-				<AppBar position="static">
-					<Toolbar>
-						<IconButton edge="start" color="inherit" aria-label="Menu">
-							<MenuIcon />
-						</IconButton>
-						<Typography variant="h6">
-							{this.printVersion()}
-						</Typography>
-						<Button
-							color="inherit"
-							onClick={this.onPublishClick}
-						>
-							Опубликовать
-						</Button>
-					</Toolbar>
-				</AppBar>
+				<div className={classes.root}>
+					<AppBar position="static">
+						<Toolbar>
+							<IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="Menu">
+								<MenuIcon />
+							</IconButton>
+							<Typography variant="h6" className={classes.title}>
+								{this.printVersion()}
+							</Typography>
+							<Button
+								onClick={this.onPublishClick}
+								color="inherit"
+							>
+								Опубликовать
+							</Button>
+						</Toolbar>
+					</AppBar>
+				</div>
 			</div>
 		);
 	}
 }
+
+export default withStyles(styles)(App);
