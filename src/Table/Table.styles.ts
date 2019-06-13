f62006439cf2252e44fb484f6931af0198d25e63
Yury Baykov <y.baykov@corp.mail.ru>
2019-06-13 04:27:08 +0300

    symptoms & managing long strings

--- src/Table/Table.styles.ts
+++ src/Table/Table.styles.ts
@@ -4,5 +4,10 @@ export default (theme: Theme) => createStyles({
 	container: {
 		height: '100%',
 		width: '100%'
+	},
+	cell: {
+		whiteSpace: 'nowrap',
+		overflow: 'hidden',
+		textOverflow: 'ellipsis'
 	}
 });
--- src/Table/Table.tsx
+++ src/Table/Table.tsx
@@ -6,7 +6,7 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-import { bodyParts, parseData } from './Table.data';
+import { bodyParts, symptoms, parseData } from './Table.data';
 import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
 
@@ -20,14 +20,14 @@ interface State {
 
 class Table extends React.Component<Props, State> {
 	public render(): React.ReactElement {
-		const [headerData, ...tableData]: string[][] = parseData(bodyParts);
+		const [headerData, ...tableData]: string[][] = parseData(symptoms);
 
 		return (
 			<MaterialUiTable>
 				<TableHead>
 					<TableRow>
 						{headerData.map((headerCell: string) => (
-							<TableCell align="right" key={headerCell}>
+							<TableCell align="right" key={headerCell} size="small">
 								{headerCell}
 							</TableCell>
 						))}
@@ -38,8 +38,12 @@ class Table extends React.Component<Props, State> {
 					{tableData.map((rowData: string[], rowIndex: number) => (
 						<TableRow key={rowIndex}>
 							{rowData.map((cellData: string, cellIndex: number) => (
-								<TableCell key={`${rowIndex}_${cellIndex}`} align="right">
-									{cellData}
+								<TableCell
+									key={`${rowIndex}_${cellIndex}`}
+									align="right"
+									size="small"
+								>
+									{(cellData.length > 20) ? `${cellData.slice(0, 20)}...` : cellData}
 								</TableCell>
 							))}
 							<TableCell align="right">
